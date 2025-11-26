d74894d4aeebb6b3f853588e84c643404d8f3894
DESKTOP-SYLVAIN\sylva <gameanalytics.hacking313@passmail.net>
2025-11-26 13:46:27 +0100

    feat: enhanced resume with print button, improved localization text, loading animation, and refined layout styling

--- resume/resume.js
+++ resume/resume.js
@@ -2,6 +2,10 @@ let current_localization = "en";
 
 let localization_path = "./localization.json"
 
+let localization_json = undefined;
+
+let localization_button = [];
+
 // Trigger printing instead of saving the web page
 document.addEventListener(
   "keydown",
@@ -25,16 +29,62 @@ async function readJson(path) {
 }
 
 document.addEventListener("DOMContentLoaded", async () => {
+
+    let loadingIcon = document.getElementById("loading-icon");
+    document.body.appendChild(loadingIcon);
+
+    let walker = document.createTreeWalker(
+        document.getElementById("resume"),
+        NodeFilter.SHOW_ELEMENT,
+        {
+            acceptNode(node) {
+                if (node === loadingIcon) {
+                    return NodeFilter.FILTER_REJECT;
+                }
+
+                return NodeFilter.FILTER_ACCEPT;
+            }
+        }
+    );
+
+    let node = walker.currentNode;
+    while (node) {
+        node.style.visibility = 'hidden';
+        node.visibility = 'hidden';
+
+        node = walker.nextNode();
+    }
+
+    await new Promise(resolve => setTimeout(resolve, 5000));
+
     await apply_localization(current_localization);
 
     let buttons = document.querySelectorAll("button");
     buttons.forEach((button) => {
         if (button.hasAttribute("setlang")) {
+            localization_button.push(button);
             button.addEventListener("click", async () => {
                 await apply_localization(button.getAttribute("setlang"));
             });
         }
     });
+
+    let print_button = document.getElementById("print");
+    if (print_button) {
+        print_button.addEventListener("click", () => {
+            window.print()
+        });
+    }
+
+    node = walker.currentNode;
+    while (node) {
+        node.style.visibility = 'visible';
+        node.visibility = 'visible';
+
+        node = walker.previousNode();
+    }
+
+    loadingIcon.remove();
 });
 
 async function apply_localization(newLocalization) {
@@ -42,16 +92,18 @@ async function apply_localization(newLocalization) {
 
     current_localization = newLocalization;
 
-    let json = await readJson(localization_path);
+    if (localization_json === undefined) {
+        localization_json = await readJson(localization_path);
+    }
 
     let localized_nodes = document.querySelectorAll("local");
     localized_nodes.forEach((node) => {
         let name = node.getAttribute("name");
 
         if (name !== undefined && name !== null && name !== "") {
-            if (json[name]) {
-                if (json[name][current_localization]) {
-                    node.innerHTML = json[name][current_localization];
+            if (localization_json[name]) {
+                if (localization_json[name][current_localization]) {
+                    node.innerHTML = localization_json[name][current_localization];
                 } else {
                     console.error("Localization " + current_localization + " not found in: " + name);
                 }
